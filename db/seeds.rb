397dda38e3a631c44e6b7aed587173824aef6f74
louiseleighhh <louisecapala@gmail.com>
2022-07-07 01:32:55 +0900

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+puts 'db is clean'
+4.times do
+  flat = Flat.create!(
+    name: Faker::TvShows::RuPaul.queen,
+    address: Faker::Games::Pokemon.location,
+    description: Faker::Quote.matz,
+    price_per_night: rand(75..100),
+    number_of_guests: rand(3..8)
+  )
+  puts "flat #{flat.id} is created"
+end
