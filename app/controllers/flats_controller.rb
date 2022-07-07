88bc094ba91eece73cfad687c2ef56bab7560ed6
louiseleighhh <louisecapala@gmail.com>
2022-07-07 21:36:16 +0900

    it works but the nav is ugly

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,54 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, only: %i[show edit update destroy]
+
+  # def search
+  #   query = params[:query]
+  #   @flats = Flat.where("name LIKE %#{query}%")
+  # end
+
+  def index
+    query = params[:query]
+    if params[:query].present?
+      @flats = Flat.where("name LIKE ?", "%#{query}%")
+    else
+      @flats = Flat.all
+    end
+  end
+
+  def show; end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night, :picture_url)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ puts 'db is clean'
     name: Faker::TvShows::RuPaul.queen,
     address: Faker::Games::Pokemon.location,
     description: Faker::Quote.matz,
+    picture_url: ['https://images.unsplash.com/photo-1499955085172-a104c9463ece?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80','https://images.unsplash.com/photo-1628745277874-919d8f8ed03a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80','https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 'https://images.unsplash.com/photo-1560185009-5bf9f2849488?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 'https://images.unsplash.com/photo-1600585152220-90363fe7e115?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 'https://images.unsplash.com/photo-1600585154084-4e5fe7c39198?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'].sample,
     price_per_night: rand(75..100),
     number_of_guests: rand(3..8)
   )
